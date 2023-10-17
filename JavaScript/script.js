fd201d3448e074d999caa37d22fde1778f930eec
jrvalerio <amarovsjr81@gmail.com>
2023-10-17 10:48:48 -0300

    17/10/2023

---
+++ JavaScript/script.js
@@ -0,0 +1,60 @@
+console.log("Hello Word!!!")
+console.log("JrValério")
+
+// variáveis
+
+// let (PODE ser reatribuido o valor)
+
+let nome = "JrValério"
+console.log(nome)
+
+// Reatribuir o valor de uma variável
+
+nome = "Amaro Jr"
+console.log(nome)
+
+let num1 = 10
+let num2 = 5
+
+let total = num1 + num2
+
+console.log(10 + 5)
+console.log(num1 + num2)
+console.log(total)
+
+// Const
+// Variável NÃO pode ser reatibuido valor
+
+const cpf = "123456789-00"
+
+// cpf = "123456789-00"
+
+console.log(cpf)
+
+// VAR NÃO UTILIZAMOS MAIS
+
+// Tipos de variáveis
+// 1 Inteiro (number)
+
+let idade = 19
+console.log(idade)
+let numero = 10000
+console.log(numero)
+
+// 2 - Float (number)
+
+let peso = 85.5
+console.log(peso)
+
+// 3 - String (string)
+
+let sobrenome = "Valério"
+console.log(sobrenome)
+let texto = "Hello Word! Bem vindos a nossa demo de JS."
+console.log(texto)
+
+// 4 - Boleano
+
+let verdadeiro = true
+console.log(verdadeiro)
+let falso = false
