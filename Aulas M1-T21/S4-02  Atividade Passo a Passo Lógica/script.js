a402e302d403287564e35c418bee350d8fc04afa
jrvalerio <amarovsjr81@gmail.com>
2023-10-19 11:38:54 -0300

    19/10/2023

---
+++ Aulas M1-T21/S4-02  Atividade Passo a Passo Lógica/script.js
@@ -0,0 +1,67 @@
+// Tarefa: Fazer um bolo de chocolate com cobertura
+// Entradas: Ingredientes, batedeira, forma.
+// Processamento:
+// - Ligue o forno em temperatura média.
+// - Coloque a manteiga na batedeira.
+// - Coloque o açúcar na batedeira.
+// - Ligue a batedeira.
+// - Enquanto houver gemas, junte uma gema e depois bata até obter
+// um creme homogêneo
+// - Adicione aos poucos o leite.
+// - Desligue a batedeira.
+// - Adicione a farinha de trigo.
+// - Adicione o chocolate em pó.
+// - Adicione o fermento.
+// - Reserve a massa obtida em um lugar temporário.
+// - Execute o algoritmo para obter as claras em neve.
+// - Junte as claras em neve à massa de chocolate que estava reservada.
+// - Misture esta massa delicadamente.
+// - Execute o algoritmo para untar a forma com manteiga e farinha.
+// - Coloque a massa na forma.
+// - Coloque a forma no forno.
+// - Espere 30 minutos.
+// - Tire a forma do forno.
+// - Desenforme o bolo ainda quente.
+// - Separe o bolo em um lugar temporário.
+// - Faça a cobertura segundo o algoritmo de fazer cobertura.
+// - Coloque a cobertura no bolo.
+// Saída: Bolo pronto para consumo.
+
+// alert("Vamos fazer um bolo?!")
+// prompt("Qual os ingredientes vc têm?")
+// prompt("Digite sua senha para atualizar:")
+
+// let nome = prompt("Digite seu nome: ")
+// let idade = prompt("digite sua idade:")
+
+// function condicao(){
+
+// if(idade > 18){
+//     alert(`${nome} tem ${idade} anos, e está apto para atualizar seu antivirus`)
+// }else{
+//     alert(`${nome} tem ${idade} anos, procure um adulto e refaça seu pedido`)
+// }
+// }
+// condicao()
+
+// let calculaIMC = function(altura, peso) {
+//     let imc = peso / (altura * altura);
+//     return imc;
+//     };
+    
+
+
+
+let peso = 82;
+let altura = 1.73;
+ 
+let imc = peso / (altura * altura);
+console.log(imc)
+
+// mostra("meu IMC é: " + imc);
+// mostra("Ainda estou " + (imc - 18.5)
+// + " pontos acima do limite da magreza.");
+
+// return imc;
+// }
+// console.log(imc)
