8472c820e6e2f26fc53cf4f760fa18bf2fbc3eaa
jrvalerio <amarovsjr81@gmail.com>
2023-10-23 07:48:22 -0300

    S4-02 💪 Atividade Passo a Passo Lógica

--- Aulas M1-T21/S4-02  Atividade Passo a Passo Lógica/script.js
+++ Aulas M1-T21/S4-02  Atividade Passo a Passo Lógica/script.js
@@ -27,190 +27,60 @@
 // - Coloque a cobertura no bolo.
 // Saída: Bolo pronto para consumo.
 
-// Função para fazer o bolo de chocolate com cobertura
-function fazerBoloDeChocolateComCobertura() {
-    // Ligue o forno em temperatura média
-    ligarForno(180);
-  
-    // Preparação da massa
-    let massa = prepararMassa();
-  
-    // Prepare as claras em neve
-    let clarasEmNeve = prepararClarasEmNeve();
-  
-    // Misture as claras em neve à massa
-    let boloPronto = misturarMassaComClaras(massa, clarasEmNeve);
-  
-    // Unte a forma com manteiga e farinha
-    untarForma();
-  
-    // Coloque a massa na forma
-    colocarMassaNaForma(boloPronto);
-  
-    // Asse o bolo no forno
-    assarBolo();
-  
-    // Desenforme o bolo
-    let boloDesenformado = desenformarBolo(boloPronto);
-  
-    // Faça a cobertura
-    let cobertura = fazerCobertura();
-  
-    // Coloque a cobertura no bolo
-    cobrirBolo(boloDesenformado, cobertura);
-  
-    // Retorne o bolo pronto
-    return boloDesenformado;
-  }
-  
-  // Funções auxiliares
-  function ligarForno(temperatura) {
-    // Implemente a lógica para ligar o forno na temperatura desejada
-  }
-  
-  function prepararMassa() {
-    // Implemente a lógica para preparar a massa do bolo
-  }
-  
-  function prepararClarasEmNeve() {
-    // Implemente a lógica para preparar as claras em neve
-  }
-  
-  function misturarMassaComClaras(massa, claras) {
-    // Implemente a lógica para misturar a massa com as claras
-  }
-  
-  function untarForma() {
-    // Implemente a lógica para untar a forma com manteiga e farinha
-  }
-  
-  function colocarMassaNaForma(massa) {
-    // Implemente a lógica para colocar a massa na forma
-  }
-  
-  function assarBolo() {
-    // Implemente a lógica para assar o bolo no forno
-  }
-  
-  function desenformarBolo(bolo) {
-    // Implemente a lógica para desenformar o bolo
-  }
-  
-  function fazerCobertura() {
-    // Implemente a lógica para fazer a cobertura do bolo
-  }
-  
-  function cobrirBolo(bolo, cobertura) {
-    // Implemente a lógica para cobrir o bolo com a cobertura
-  }
-  
 
-  // Função para ligar o forno na temperatura desejada
-function ligarForno(temperatura) {
-  console.log(`Ligando o forno a ${temperatura}°C.`);
-  // Lógica para ligar o forno na temperatura desejada
+console.log("Tarefa: Fazer um bolo de chocolate com cobertura");
+
+// Entradas: Ingredientes, batedeira, forma.
+
+console.log("Ligue o forno em temperatura média.");
+console.log("Coloque a manteiga na batedeira.");
+console.log("Coloque o açúcar na batedeira.");
+console.log("Ligue a batedeira.");
+
+// Processamento:
+function fazerBolo() {
+  let gemas = 3;
+  let leite = 1;
+  let clarasEmNeve = true;
+
+  while (gemas > 0) {
+    console.log("Junte uma gema e bata até obter um creme homogêneo.");
+    gemas--;
+  }
+
+  console.log("Adicione aos poucos o leite.");
+  console.log("Desligue a batedeira.");
+  console.log("Adicione a farinha de trigo.");
+  console.log("Adicione o chocolate em pó.");
+  console.log("Adicione o fermento.");
+  console.log("Reserve a massa obtida em um lugar temporário.");
+
+  if (clarasEmNeve) {
+    console.log("Execute o algoritmo para obter as claras em neve.");
+  }
+
+  console.log(
+    "Junte as claras em neve à massa de chocolate que estava reservada."
+  );
+  console.log("Misture esta massa delicadamente.");
+
+  // Executar algoritmo para untar a forma com manteiga e farinha.
+  console.log("Unte a forma com manteiga e farinha.");
+  console.log("Coloque a massa na forma.");
+  console.log("Coloque a forma no forno.");
+  console.log("Espere 30 minutos.");
+  console.log("Tire a forma do forno.");
+  console.log("Desenforme o bolo ainda quente.");
+
+  let bolo = "Bolo pronto para consumo.";
+  return bolo;
 }
 
-// Função para preparar a massa do bolo
-function prepararMassa() {
-  console.log("Preparando a massa do bolo.");
-  // Lógica para preparar a massa
-  const massa = {
-    manteiga: '100g',
-    acucar: '200g',
-    // Outros ingredientes da massa
-  };
-  return massa;
-}
+const boloPronto = fazerBolo();
 
-// Função para preparar as claras em neve
-function prepararClarasEmNeve() {
-  console.log("Preparando claras em neve.");
-  // Lógica para preparar as claras em neve
-  const claras = {
-    quantidade: 4,
-    consistencia: 'em neve',
-  };
-  return claras;
-}
+// Faça a cobertura seguindo o algoritmo de fazer cobertura.
 
-// Função para misturar a massa com as claras
-function misturarMassaComClaras(massa, claras) {
-  console.log("Misturando a massa com as claras em neve.");
-  // Lógica para misturar a massa com as claras
-  const boloPronto = {
-    massa,
-    claras,
-  };
-  return boloPronto;
-}
+console.log("Coloque a cobertura no bolo.");
 
-// Função para untar a forma com manteiga e farinha
-function untarForma() {
-  console.log("Untando a forma com manteiga e farinha.");
-  // Lógica para untar a forma
-}
-
-// Função para colocar a massa na forma
-function colocarMassaNaForma(boloPronto) {
-  console.log("Colocando a massa na forma.");
-  // Lógica para colocar a massa na forma
-}
-
-// Função para assar o bolo no forno
-function assarBolo() {
-  console.log("Assando o bolo no forno.");
-  // Lógica para assar o bolo no forno
-}
-
-// Função para desenformar o bolo
-function desenformarBolo(boloPronto) {
-  console.log("Desenformando o bolo.");
-  // Lógica para desenformar o bolo
-  return boloPronto; // Retornando o bolo desenformado
-}
-
-// Função para fazer a cobertura do bolo
-function fazerCobertura() {
-  console.log("Preparando a cobertura do bolo.");
-  // Lógica para preparar a cobertura
-  const cobertura = {
-    chocolate: '100g',
-    cremeDeLeite: '200g',
-    acucarDeConfeiteiro: '50g',
-    // Outros ingredientes da cobertura
-  };
-  return cobertura;
-}
-
-// Função para cobrir o bolo com a cobertura
-function cobrirBolo(bolo, cobertura) {
-  console.log("Cobrindo o bolo com a cobertura.");
-  // Lógica para cobrir o bolo com a cobertura
-  const boloCoberto = {
-    ...bolo,
-    cobertura,
-  };
-  return boloCoberto;
-}
-
-// Função principal para fazer o bolo de chocolate com cobertura
-function fazerBoloDeChocolateComCobertura() {
-  ligarForno(180);
-  const massa = prepararMassa();
-  const clarasEmNeve = prepararClarasEmNeve();
-  const boloPronto = misturarMassaComClaras(massa, clarasEmNeve);
-  untarForma();
-  colocarMassaNaForma(boloPronto);
-  assarBolo();
-  const boloDesenformado = desenformarBolo(boloPronto);
-  const cobertura = fazerCobertura();
-  const boloFinal = cobrirBolo(boloDesenformado, cobertura);
-  return boloFinal;
-}
-
-// Chamar a função principal para fazer o bolo
-const boloProntoParaConsumo = fazerBoloDeChocolateComCobertura();
-console.log("Bolo pronto para consumo:", boloProntoParaConsumo);
-
-
+// Saída: Bolo pronto para consumo.
+console.log(boloPronto);
