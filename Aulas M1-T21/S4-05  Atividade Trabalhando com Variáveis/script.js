e181f1a41240b27995ff22339472e80e39fac552
jrvalerio <amarovsjr81@gmail.com>
2023-10-23 22:15:27 -0300

    S4-05 | 💪 Atividade: Trabalhando com Variáveis

---
+++ Aulas M1-T21/S4-05  Atividade Trabalhando com Variáveis/script.js
@@ -0,0 +1,47 @@
+// Faça um programa que exiba um decimal, um inteiro e uma string utilizando o console.log().
+
+// Declaração de variáveis
+let numeroDecimal = 3.14;
+let numeroInteiro = 42;
+let texto = "Olá, mundo!";
+
+// Exibindo as variáveis no console
+console.log("Número Decimal:", numeroDecimal);
+console.log("Número Inteiro:", numeroInteiro);
+console.log("String:", texto);
+
+
+// Declare uma variável, atribua um número, use o método typeof para mostrar na tela a palavra "number".
+
+const num = 199;
+
+console.log(typeof num);
+
+// Declare uma variável, atribua um valor booleano, use o método typeof para mostrar na tela a palavra "boolean".
+
+const flag = true;
+
+console.log(typeof flag);
+
+// Declare uma variável, atribua uma string, use o método typeof para mostrar na tela a palavra "string".
+
+const str = "String";
+
+console.log(typeof str);
+
+//Faça um programa que exiba duas variáveis, uma com atribuição true e outra false
+
+let verdadeiro = true;
+console.log(verdadeiro);
+let falso = false;
+console.log(falso);
+
+// Usando uma variável, e o console.log(), mostre no console o valor 'null'
+
+let naoDeclarada;
+console.log();
+
+// Usando uma variável, e o console.log(), mostre no console o valor 'undefined'
+
+let meuNumero;
+console.log(meuNumero);
