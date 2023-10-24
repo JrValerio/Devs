04cf4347e51274286489a0b22c7ae78d43cc072a
jrvalerio <amarovsjr81@gmail.com>
2023-10-24 10:52:07 -0300

    S4-06 | 📘 Aula: Operadores Aritméticos

---
+++ Aulas M1-T21/S4-06  Aula Operadores Aritméticos/script.js
@@ -0,0 +1,65 @@
+// Operadores Aritméticos Primários
+// Alguns exemplos de operadores aritméticos em JavaScript incluem:
+
+// Soma (+)
+const a = 10; // Atribuindo 10 a variável a
+const b = 20; // Atribuindo 20 a variável b
+const soma = a + b; // Somando os valores de a e b.
+console.log(soma); // Saída: 30
+
+// Subtração (-)
+const c = 10; // Atribuindo 10 a variável c
+const d = 20; // Atribuindo 20 a variável d
+const subtracao = d - c; // Subtraindo o valor c de d.
+console.log(subtracao); // Saída: 10
+
+// Divisão (/)
+const e = 10; // Atribuindo 10 a variável e
+const f = 2; // Atribuindo 2 a variável f
+const divisao = e / f; // Dividindo e por f.
+console.log(divisao); // Saída: 5
+
+// Multiplicação (*)
+const g = 10; // Atribuindo 10 a variável g
+const h = 20; // Atribuindo 20 a variável h
+const multiplicacao = g * h; // Multiplicando o valor de g por h.
+console.log(multiplicacao); // Saída: 200
+
+// Módulo (%)
+const i = 10; // Atribuindo 10 a variável i
+const j = 3; // Atribuindo 3 a variável j
+const restoDaDivisao = i % j; // Capturando o resto da divisão entre i e j.
+console.log(restoDaDivisao); // Saída: 1
+
+// Exponenciação (**)
+const k = 2; // Atribuindo 2 a variável k
+const exponenciacao = k ** 3; // Elevando k (2) ao cubo (^3).
+console.log(exponenciacao); // Saída: 8
+// Equivalente a -> exponenciacao = k * k * k
+
+// Operadores de Atribuição
+// Combinando operadores
+
+let x = 5; // Atribuindo 5 a variável x
+x += 3; // Equivale a -> x = x + 3
+console.log(x); // Saída: 8
+
+let y = 5; // Atribuindo 5 a variável y
+y -= 3; // Equivale a -> y = y - 3
+console.log(y); // Saída: 2
+
+let z = 2; // Atribuindo 2 a variável z
+z *= 10; // Equivale a -> z = z * 10
+console.log(z); // Saída: 20
+
+let b = 100; // Atribuindo 100 a variável b
+b /= 4; // Equivale a b = b / 4
+console.log(b); // Saída: 25
+
+let c = 10; // Atribuindo 10 a variável c
+c %= 3; // Equivale a c = c % 3
+console.log(c); // Saída: 1
+
+let d = 5; // Atribuindo 5 a variável d
+d **= 3; // Equivale a d = d ** 3
+console.log(d); // Saída: 125
