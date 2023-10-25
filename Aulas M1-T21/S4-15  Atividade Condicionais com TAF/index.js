4e6ffcd1b8a6170ec59488b1472d084b0cf2061a
jrvalerio <amarovsjr81@gmail.com>
2023-10-25 18:00:34 -0300

    S4-15 | 💪 Atividade: Condicionais com TAF

---
+++ Aulas M1-T21/S4-15  Atividade Condicionais com TAF/index.js
@@ -0,0 +1,37 @@
+// Não altere o nome das variavéis ou o texto do prompt!
+// Modifique apenas o tipo da variavel!
+const gender = prompt("Genero");
+const height = prompt("Altura");
+const barReps = prompt("Repetições com barra");
+const barSeconds = prompt("Tempo das repetições com barra");
+const abs = prompt("Abdominais");
+const runDistance = prompt("Distância da corrida");
+const runTime = prompt("Tempo da corrida");
+const swimDistance = prompt("Distância da natação");
+const swimTime = prompt("Tempo da natação");
+const diveTime = prompt(" Tempo de mergulho");
+
+let result = false;
+
+//Desenvolva seu código aqui:
+if (
+  gender == "male" &&
+  height >= 1.7 &&
+  (barReps >= 6 || barSeconds >= 15) &&
+  abs >= 41 &&
+  ((runDistance >= 3000 && runTime <= 720) ||
+    (runDistance >= 5000 && runTime <= 1200)) &&
+  ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30)
+) {
+  result = true;
+} else if (
+  gender == "female" &&
+  height >= 1.6 &&
+  (barReps >= 5 || barSeconds >= 12) &&
+  abs >= 41 &&
+  ((runDistance >= 4000 && runTime <= 900) ||
+    (runDistance >= 6000 && runTime <= 1320)) &&
+  ((swimDistance >= 100 && swimTime <= 60) || diveTime <= 30)
+) {
+  result = true;
+}
