2c8b9023b40bfabaf100e08698315b9178c5a476
jrvalerio <amarovsjr81@gmail.com>
2023-10-25 23:44:27 -0300

    S5-02  📘 Aula Sintaxe e Declaração de Funções

---
+++ Aulas M1-T21/S5-02  Aula Sintaxe e Declaração de Funções/script.js
@@ -0,0 +1,41 @@
+// function nomeFuncao() {
+//     ⁠  // bloco de código da função
+//     }
+
+function somar() {
+  const numero1 = 3;
+  const numero2 = 5;
+  console.log(numero1 + numero2);
+}
+
+function verificarMaioridade() {
+  const idade = 18;
+
+  if (idade >= 18) {
+    console.log("Maior de idade");
+  } else {
+    console.log("Menor de idade");
+  }
+}
+
+// Declaração da função
+function somar() {
+  const numero1 = 3;
+  const numero2 = 5;
+  console.log(numero1 + numero2);
+}
+
+somar(); // Chamada da função
+
+// Declaração da função
+function somar() {
+  const numero1 = 3;
+  const numero2 = 5;
+  console.log(numero1 + numero2);
+}
+
+somar(); // Chamada da função
+somar(); // Chamada da função
+somar(); // Chamada da função
+
+// Esse código irá imprimir 3 vezes a soma de 3 + 5.
